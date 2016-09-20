88291c2fe84cd193197c2aac4d85e0fa2e838810
hadoop-slave3 <hadoop@db2.slave3.com>
2016-09-20 15:59:40 +0530

    Tested

---
+++ Hello1.cob
@@ -0,0 +1,13 @@
+      *>----------------------------------------------------------------
+      *> TEST Hello
+      *>----------------------------------------------------------------
+       IDENTIFICATION   DIVISION.
+       PROGRAM-ID.      HELLO.
+       ENVIRONMENT      DIVISION.
+       CONFIGURATION    SECTION.
+       DATA             DIVISION.
+       FILE SECTION.
+      *>----------------------------------------------------------------
+       PROCEDURE DIVISION.
+           display "Msg-1 --> Hello World!"
+       STOP RUN.
